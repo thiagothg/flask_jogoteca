68fc7aa3e518df09a69481e8c14fd681f56366ca
Thiago Rodrigues <thiago_thg@outlook.com>
2022-07-09 12:01:59 -0300

    reutilizar trechos do template

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
-from flask import Flask, render_template
+from crypt import methods
+from flask import Flask, redirect, render_template, request
 from jogos import Jogo
 
 app = Flask(__name__)
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Combat', 'Flight', 'PS2')
+
+list = [jogo1, jogo2, jogo3]
+
 @app.route('/')
-
 def hello():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
-    jogo3 = Jogo('Mortal Combat', 'Flight', 'PS2')
-
-    list = [jogo1, jogo2, jogo3]
     return render_template('list.html', titulo='Jogos', jogos=list)
 
+@app.route('/new')
+def new():
+    return render_template('new.html', titulo='Novo')
 
+@app.route('/create', methods=['POST',])
+def create():
+    name = request.form['name']
+    category = request.form['category']
+    console = request.form['console']
+    
+    jogo = Jogo(name, category, console)
+    
+    list.append(jogo)
+    return redirect('/')
+    
 
 app.run(debug=True)
