212295494a300b82704ee0e66940bb947e8a8450
Thiago Rodrigues <thiago_thg@outlook.com>
2022-07-10 20:13:16 -0300

    tela de login & guardar os dados da sessão.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from crypt import methods
-from flask import Flask, redirect, render_template, request
+from flask import Flask, flash, redirect, render_template, request, session
 from jogos import Jogo
 
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
 jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
@@ -11,8 +11,11 @@ jogo3 = Jogo('Mortal Combat', 'Flight', 'PS2')
 list = [jogo1, jogo2, jogo3]
 
 @app.route('/')
-def hello():
+def index():
     return render_template('list.html', titulo='Jogos', jogos=list)
+    # if session['userLoggedIn'] == True:
+    # else:
+    #     return redirect('/login')
 
 @app.route('/new')
 def new():
@@ -28,6 +31,26 @@ def create():
     
     list.append(jogo)
     return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', titulo='Login')
     
 
+@app.route('/autenticate', methods=['POST',])
+def autenticate(): 
+    if 'user' == request.form['user']:
+        session['userLoggedIn'] = True
+        flash('{} logon success'.format(request.form['user']))
+        return redirect('/')
+    else:
+        flash('try again', category='error')
+        return redirect('/login')
+    
+@app.route('/logout')
+def logout():
+    session['userLoggedIn'] = False
+    flash('User logout success!')
+    return redirect('/')
+    
 app.run(debug=True)
