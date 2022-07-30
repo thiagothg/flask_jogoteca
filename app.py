938d0490184236e4aa14574396c1c9a92258f303
Thiago Rodrigues <thiago_thg@outlook.com>
2022-07-30 14:46:20 -0300

    update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 from jogos import Jogo
+from user import User
 
 app = Flask(__name__)
 app.secret_key = 'alura'
@@ -10,8 +11,19 @@ jogo3 = Jogo('Mortal Combat', 'Flight', 'PS2')
 
 list = [jogo1, jogo2, jogo3]
 
+user1 = User("Bruno Divino", "BD", "alohomora")
+user2 = User("Camila Ferreira", "Mila", "paozinho")
+user3 = User("Guilherme Louro", "Cake", "Python_eh_vida")
+
+users = { user1.nickname : user1,
+             user2.nickname : user2,
+             user3.nickname : user3 }
+
 @app.route('/')
 def index():
+    print(session)
+    if 'user_logged' not in session or session['user_logged'] == None:
+        redirect(url_for('login'))
     return render_template('list.html', titulo='Jogos', jogos=list)
     # if session['userLoggedIn'] == True:
     # else:
@@ -19,6 +31,8 @@ def index():
 
 @app.route('/new')
 def new():
+    if 'user_logged' not in session or session['user_logged'] == None:
+        return redirect(url_for('login'))
     return render_template('new.html', titulo='Novo')
 
 @app.route('/create', methods=['POST',])
@@ -30,27 +44,37 @@ def create():
     jogo = Jogo(name, category, console)
     
     list.append(jogo)
-    return redirect('/')
+    return redirect(url_for('index'))
 
 @app.route('/login')
 def login():
-    return render_template('login.html', titulo='Login')
+    print(session)
+    if 'user_logged' in session:
+        return redirect(url_for('index'))
+    next_page = request.args.get('next')
+    return render_template('login.html', titulo='Login', next_page=next_page)
     
 
 @app.route('/autenticate', methods=['POST',])
 def autenticate(): 
-    if 'user' == request.form['user']:
-        session['userLoggedIn'] = True
-        flash('{} logon success'.format(request.form['user']))
-        return redirect('/')
+    if request.form['user'] in users:
+        user = users[request.form['user']]
+        if request.form['password'] == user.password:
+            session['user_logged'] = user.nickname
+            flash(user.nickname + ' logado com sucesso!')
+            next_page = request.form['next_page']
+            print(next_page)
+            return redirect(next_page)
+        else:
+            return redirect(url_for('login'))
     else:
-        flash('try again', category='error')
-        return redirect('/login')
-    
+        flash('Usuário não logado.', category='error')
+        return redirect(url_for('login'))
+        
 @app.route('/logout')
 def logout():
-    session['userLoggedIn'] = False
+    session['user_logged'] = None
     flash('User logout success!')
-    return redirect('/')
+    return redirect(url_for('login'))
     
 app.run(debug=True)
---
+++ user.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, name, nickname, password):
+        self.nome = name
+        self.nickname = nickname
+        self.password = password
